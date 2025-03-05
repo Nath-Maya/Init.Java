1f113d47e7d06a942b3d5573d5ff0ce9c524c4e9
User <user@Users-MBP-3.lan>
2025-03-04 22:57:19 -0500

    Defining attributes and methods of the Shop class

--- activity_4/shop/src/App.java
+++ activity_4/shop/src/App.java
@@ -1,6 +1,9 @@
 public class App {
-    public static void main(String[] args)  {
-        Shop shop = new Shop();
-        shop.mostrarMensaje();
+    public static void main(String[] args) {
+        Shop tienda = new Shop("Variedades Nathalia", "Calle 75# 7 tbis 05 ", 320808776, "9 AM - 9 PM");
+
+        tienda.addToCart("Iphone");
+        tienda.addToCart("Teclado");
+        tienda.showCart();
     }
 }
--- activity_4/shop/src/Shop.java
+++ activity_4/shop/src/Shop.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop {
-    public void mostrarMensaje() {
-        System.out.println("Bienvenido a la tienda");
+
+    //Atributos
+    private String name;
+    private String address;
+    private int phone;
+    private String schedule;
+    private List<String> cart; 
+
+    //Constructor
+    public Shop(String name, String address, int phone, String schedule) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.schedule = schedule;
+        this.cart = new ArrayList<>(); 
+    }
+
+    //Metodos Getters y Setters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    //Metodos
+    public void addToCart(String item) {
+        cart.add(item);
+        System.out.println(item + " ha sido agregado al carrito.");
+    }
+
+    public void showCart() {
+        if (cart.isEmpty()) {
+            System.out.println("El carrito está vacío.");
+        } else {
+            System.out.println("Artículos en el carrito: " + cart);
+        }
     }
 }
